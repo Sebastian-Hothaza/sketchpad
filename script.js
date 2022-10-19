af2408e0fa5d11934c3c487accbd87e8e7d505c7
Sebastian <sebastianhothaza@gmail.com>
2022-10-19 16:06:31 -0400

    Added hover functionality

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function handleUserSelection(btn){
     console.log("User clicked on " + btn);
     switch (btn){
         case "penButton":
-            drawboard(16);
+            drawboard(8);
             break;
         case "eraserButton":
             document.querySelector('#sketchPad').innerHTML='';
@@ -31,19 +31,35 @@ function handleUserSelection(btn){
 // Given a size, draws the grid by attaching divs as required to the HTML
 // Note: The "tile" formatting is described in JS, NOT in CSS!
 function drawboard(size){
-    const gridDimension = 600;
+    const gridDimension = 600; // TODO: This is ugly, should be cleaned up later
 
     // Get the sketchpad
     const padHook = document.querySelector('#sketchPad');
     // Remove anything attached to it
     padHook.innerHTML='';
 
+
     // Creation of the divs. We use float.
     for (let i=0; i<size*size; i++){
         const square = document.createElement('div');
 
-        // Styling of the divs TODO: Remove 600 hardcode!
-        square.style.cssText = `height:${(gridDimension-2*size)/size}px; width:${(gridDimension-2*size)/size}px; border-style: solid; border-width: 1px; background-color: ${backgroundColor}; float:left`
+
+
+
+
+
+
+
+        // Styling of the divs 
+        square.style.cssText = `height:${(gridDimension-2*size)/size}px; width:${(gridDimension-2*size)/size}px;`
+        // Assigning the squares an ID so that we can define hover property in CSS
+        square.classList.add('gridSquare')
+
+
+
+
+
+
 
 
         // Add to the padS
